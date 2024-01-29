67164980d0f711239b854a06a7568909a45c9635
synsan123 <68645250+synsan123@users.noreply.github.com>
2024-01-29 11:04:36 +0900

    Add files via upload

---
+++ 240129/Basic_source_code/hanbaek_basic_source(Raspberry)/led/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <unistd.h>
+#include <time.h>
+#define LED_RED 7
+#define LED_GREEN 21
+#define LED_BLUE 22
+int main(void){
+	int i;
+	if(wiringPiSetup () == -1)
+		return 1;
+	pinMode(LED_RED,OUTPUT);
+	pinMode(LED_GREEN,OUTPUT);
+	pinMode(LED_BLUE,OUTPUT);
+	digitalWrite(LED_RED,0);
+	digitalWrite(LED_GREEN,0);
+	digitalWrite(LED_BLUE,0);
+	printf("3 Color LED Control Start !! \n");
+	for(i=0;i<20;i++){
+		printf("Red LED On !! \n");
+		digitalWrite(LED_RED,1);
+		usleep(500000);
+		printf("Red LED Off !! \nGreen LED On !!\n");
+		digitalWrite(LED_RED,0);
+		digitalWrite(LED_GREEN,1);
+		usleep(500000);
+		printf("Green LED Off !! \nBlue LED On !!\n");
+		digitalWrite(LED_GREEN,0);
+		digitalWrite(LED_BLUE,1);
+		usleep(500000);
+		printf("Blue LED Off !! \n");
+		digitalWrite(LED_BLUE,0);
+	}
+	return 0;
+}
